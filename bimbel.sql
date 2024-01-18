d930d295f91ec1cb81d5bdc0dcac9766b046fb68
Fadilano <fadilanoa@gmail.com>
2024-01-18 14:56:02 +0700

    database new

--- bimbel.sql
+++ bimbel.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 17, 2024 at 01:47 PM
--- Server version: 5.6.26
--- PHP Version: 5.6.12
+-- Waktu pembuatan: 18 Jan 2024 pada 08.53
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,17 +24,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Struktur dari tabel `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
+CREATE TABLE `admin` (
   `kode_guru` int(11) NOT NULL,
   `username` varchar(25) NOT NULL,
   `password` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `admin`
+-- Dumping data untuk tabel `admin`
 --
 
 INSERT INTO `admin` (`kode_guru`, `username`, `password`) VALUES
@@ -42,102 +43,144 @@ INSERT INTO `admin` (`kode_guru`, `username`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_datasiswa`
+-- Struktur dari tabel `tbl_datasiswa`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_datasiswa` (
-  `npm` int(11) NOT NULL,
+CREATE TABLE `tbl_datasiswa` (
+  `id_siswa` int(10) NOT NULL,
   `nama` varchar(30) NOT NULL,
   `kelas` varchar(15) NOT NULL,
   `sekolah` varchar(30) NOT NULL,
   `alamat` varchar(35) NOT NULL,
   `telepon` char(13) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `tbl_datasiswa`
+-- Dumping data untuk tabel `tbl_datasiswa`
 --
 
-INSERT INTO `tbl_datasiswa` (`npm`, `nama`, `kelas`, `sekolah`, `alamat`, `telepon`) VALUES
+INSERT INTO `tbl_datasiswa` (`id_siswa`, `nama`, `kelas`, `sekolah`, `alamat`, `telepon`) VALUES
 (50421436, 'Fadilano', '8', 'SMA YADIKA 4', 'Legok', '085883327454'),
 (50421437, 'Abraham', '2', 'SD anjay', 'Bekasi', '0125123465'),
-(242452235, 'Fadilsdasfas', '2', 'SD anjay', 'Bekasi', '0125123465'),
-(51421104, 'muzi', '6', 'SDN 05', 'kampung ', '');
+(51421104, 'muzi', '6', 'SDN 05', 'kampung ', ''),
+(242452235, 'Fadilsdasfas', '2', 'SD anjay', 'Bekasi', '0125123465');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_laporan`
+-- Struktur dari tabel `tbl_laporan`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_laporan` (
+CREATE TABLE `tbl_laporan` (
   `id` int(10) NOT NULL,
-  `npm` int(11) NOT NULL,
+  `id_siswa` int(10) NOT NULL,
   `tanggal` date NOT NULL,
   `matpel` varchar(25) NOT NULL,
   `materi` varchar(25) NOT NULL,
   `penilaian` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `tbl_laporan`
+-- Dumping data untuk tabel `tbl_laporan`
 --
 
-INSERT INTO `tbl_laporan` (`id`, `npm`, `tanggal`, `matpel`, `materi`, `penilaian`) VALUES
+INSERT INTO `tbl_laporan` (`id`, `id_siswa`, `tanggal`, `matpel`, `materi`, `penilaian`) VALUES
 (3, 50421436, '2024-01-17', 'BAHASA', 'aljabar', 20),
 (4, 50421437, '2024-01-07', 'Mandarin', 'Halo', 90);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `tbl_pembayaran`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `npm` int(11) NOT NULL,
+CREATE TABLE `tbl_pembayaran` (
+  `id` int(11) NOT NULL,
+  `id_siswa` int(10) NOT NULL,
+  `bulan` varchar(20) NOT NULL,
+  `metode` varchar(50) NOT NULL,
+  `tanggal` date NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_siswa` int(10) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`npm`, `username`, `password`) VALUES
+INSERT INTO `user` (`id_siswa`, `username`, `password`) VALUES
+(50421436, 'fadilano', '1234'),
 (51421103, 'adi', 'adi123'),
-(51421104, 'muzi', 'muzi123');
+(51421104, 'Muzi', 'muzi123');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `admin`
+-- Indeks untuk tabel `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`kode_guru`);
 
 --
--- Indexes for table `tbl_laporan`
+-- Indeks untuk tabel `tbl_datasiswa`
+--
+ALTER TABLE `tbl_datasiswa`
+  ADD PRIMARY KEY (`id_siswa`);
+
+--
+-- Indeks untuk tabel `tbl_laporan`
 --
 ALTER TABLE `tbl_laporan`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `tbl_pembayaran`
+--
+ALTER TABLE `tbl_pembayaran`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`npm`);
+  ADD PRIMARY KEY (`id_siswa`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `tbl_laporan`
+-- AUTO_INCREMENT untuk tabel `tbl_datasiswa`
+--
+ALTER TABLE `tbl_datasiswa`
+  MODIFY `id_siswa` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=242452236;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_laporan`
 --
 ALTER TABLE `tbl_laporan`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_pembayaran`
+--
+ALTER TABLE `tbl_pembayaran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
